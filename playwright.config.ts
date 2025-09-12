c952c468c7778c1bc08e67980d0277ef8c501d4c
Omar Elbably <omaroelbably@gmail.com>
2025-09-12 21:50:55 +0300

    updating yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false,
+    headless: true,
   },
   /* Configure projects for major browsers */
   projects: [
